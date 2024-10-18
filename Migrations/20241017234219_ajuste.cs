cf3614f70852aaaa46203c425cc60dc14005f1a5
MarcosMartinsDeLima <0timetraveler@gmail.com>
2024-10-17 21:21:31 -0300

    fix: altera date

---
+++ Migrations/20241017234219_ajuste.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CursoIdiomas.Migrations
+{
+    /// <inheritdoc />
+    public partial class ajuste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "DataNascimento",
+                table: "Aluno",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataNascimento",
+                table: "Aluno",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
