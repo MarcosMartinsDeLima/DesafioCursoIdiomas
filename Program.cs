f054e7953df3231615595c8a63cb02f22ffc6153
MarcosMartinsDeLima <0timetraveler@gmail.com>
2024-10-18 22:19:56 -0300

    feat: injeta dependecia de repository

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+using CursoIdiomas.Model.Context;
+using CursoIdiomas.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+//injetando repositories
+builder.Services.AddScoped<ITurmaRepository,TurmaRepository>();
+builder.Services.AddScoped<IAlunoRepository,AlunoRepository>();
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MysqlContext>(options => options.UseMySql(connection,new MySqlServerVersion(new Version(8,0,5))));
+
 
 var app = builder.Build();
 
@@ -16,29 +29,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapControllers();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
